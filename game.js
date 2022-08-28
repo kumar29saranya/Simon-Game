2fd852ebbdb03bdfca9f7625053309131f83ed39
kumar29saranya <74856353+kumar29saranya@users.noreply.github.com>
2022-08-28 21:21:41 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,93 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+
+    $("#level-title").text("Level " + level);
+    setTimeout(function(){
+        nextSequence();
+    },1000)
+    
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+  
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+  
+    checkAnswer(userClickedPattern.length-1);
+  });
+
+function checkAnswer(currentLevel){
+    console.log("answer: "+gamePattern);
+    console.log("you entered: " + userClickedPattern);
+    if(gamePattern[currentLevel]==userClickedPattern[currentLevel])
+    {
+        if(userClickedPattern.length==gamePattern.length)
+        {
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    }
+    else
+    {
+        //console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+        $("#level-title").text("Game Over, Press any key to Restart");
+        startOver();
+    }
+}
+
+function nextSequence() {
+    userClickedPattern=[];
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function startOver(){
+    level=0;
+    gamePattern=[];
+    started=false;
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed");
+    },100);
+}
+
+$(document).ready(function(){
+    $(".newbtn").click(function(){
+      $(".help").toggle();
+      $(this).html($(this).html() == 'Show Help' ? 'Hide Help' : 'Show Help');
+    });
+  });
