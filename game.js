559e4f83fb92a20721e2dae9d9f02f70eee3663f
kumar29saranya <74856353+kumar29saranya@users.noreply.github.com>
2022-11-05 15:59:16 +0530

    add landing page

--- game.js
+++ game.js
@@ -6,17 +6,28 @@ const bodyParser=require("body-parser");
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(favicon(__dirname + '/favicon.ico'));
+app.use(favicon(__dirname + '/images/favicon.ico'));
+
+app.use('/images', express.static('images'));
+app.use('/images', express.static('sounds'));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
-app.get('/styles.css', function(req, res) {
-    res.sendFile(__dirname + "/" + "styles.css");
-  });
+
+app.get('/css/styles.css', function(req, res) {
+    res.sendFile(__dirname + "/" + "css/styles.css");
+});
+
+  app.get("/gamepage.html", function(req, res){
+    res.sendFile(__dirname + "/gamepage.html");
+});
+  app.get('/css/styles2.css', function(req, res) {
+    res.sendFile(__dirname + "/" + "css/styles2.css");
+});
   app.get('/working.js', function(req, res) {
     res.sendFile(__dirname + "/" + "working.js");
-  });
+});
 
 
 app.listen(3000, function(){
